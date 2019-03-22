083452c10749a36f751e0a1b1bffc88b7409c4c7
JoniVR <joni.vanroost@student.kdg.be>
2019-03-22 16:17:04 +0100

    initial commit

---
+++ src/script.js
@@ -0,0 +1,5 @@
+const pipBtn = document.querySelector('.ytp-pip-button');
+const miniPlayerBtn = document.querySelector('.ytp-miniplayer-button');
+
+pipBtn.style.display = '';
+miniPlayerBtn.style.display = 'none';
